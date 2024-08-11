d1b0ad141a5e24748baa9f9908ca682132c858b6
Kunwar <kunwarabhay05@gmail.com>
2024-08-11 22:12:53 +0530

    boxes and reset button working

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
+let turnO= true;
+const winPattern=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+boxes.forEach((box) => {
+    box.addEventListener("click", ()=>{
+        if(turnO == true)
+        {
+            box.innerText="O";
+            turnO= false;
+        }
+        else{
+            box.innerText="X";
+            turnO= true;
+        }
+    });
+});
+
+resetBtn.addEventListener("click",()=>{
+    boxes.forEach((box) =>
+    {
+        box.innerText="";
+    })
+})
